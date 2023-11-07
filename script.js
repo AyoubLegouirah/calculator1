86274f6a5195baf217689c70118acc8a9deef362
AyoubLegouirah <ayoubgrand@outlook.fr>
2023-11-07 11:02:22 +0100

    modification script

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Sélectionnez les éléments du DOM
 const resultElement = document.getElementById('resultat');
 const buttons = document.querySelectorAll('.contenair button');
 
@@ -6,45 +5,6 @@ let currentInput = '';
 let currentOperator = '';
 let previousInput = '';
 
-// Fonction pour mettre à jour l'affichage du résultat
-function updateResult() {
-  resultElement.textContent = currentInput;
-}
-
-// Fonction pour gérer les clics sur les boutons
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const value = button.textContent;
-    if (!isNaN(value) || value === '.') {
-      // Si c'est un chiffre ou un point, ajoutez-le à l'entrée actuelle
-      currentInput += value;
-      updateResult();
-    } else if (value === 'RESET') {
-      // Réinitialisez la calculatrice
-      currentInput = '';
-      previousInput = '';
-      currentOperator = '';
-      updateResult();
-    } else if (value === '=') {
-      // Effectuez le calcul lorsque le bouton égal est cliqué
-      if (currentInput && currentOperator && previousInput) {
-        currentInput = calculate(parseFloat(previousInput), currentOperator, parseFloat(currentInput));
-        previousInput = '';
-        currentOperator = '';
-        updateResult();
-      }
-    } else {
-      // Stockez l'opérateur et l'entrée précédente
-      if (currentInput) {
-        previousInput = currentInput;
-        currentOperator = value;
-        currentInput = '';
-      }
-    }
-  });
-});
-
-// Fonction pour effectuer des calculs
 function calculate(num1, operator, num2) {
   switch (operator) {
     case '+':
@@ -59,3 +19,42 @@ function calculate(num1, operator, num2) {
       return num2;
   }
 }
+
+function updateResult() {
+  resultElement.textContent = currentInput;
+}
+
+buttons.forEach(button => {
+
+  button.addEventListener('click', () => {
+    const value = button.textContent;
+
+    if (!isNaN(value) || value === '.') {
+      currentInput = currentInput + value;
+      updateResult();
+
+    } else if (value === 'RESET') {
+      currentInput = '';
+      previousInput = '';
+      currentOperator = '';
+      updateResult();
+      
+    } else if (value === '=') {
+      if (currentInput && currentOperator && previousInput) {
+        currentInput = calculate(parseFloat(previousInput), currentOperator, parseFloat(currentInput));
+        previousInput = '';
+        currentOperator = '';
+        updateResult();
+      }
+
+    } else {
+      if (currentInput) {
+        previousInput = currentInput;
+        currentOperator = value;
+        currentInput = '';
+      }
+    }
+  });
+});
+
+
